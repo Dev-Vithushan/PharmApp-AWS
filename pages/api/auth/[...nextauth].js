6b826401df9fca29dfd2acf256f2b698e1fb3bbc
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-11-08 14:59:05 +0530

    aws services added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,6 +21,12 @@ export default NextAuth({
     },
   },
   providers: [
+    // providers.Cognito({
+    //   clientId : process.env.COGNITO_CLIENT_ID,
+    //   clientSecret : process.env.COGNITO_CLIENT_SECRET,
+    //   domain : process.env.COGNITO_DOMAIN
+
+    // }),
     CredentialsProvider({
       async authorize(credentials) {
         await db.connect();
